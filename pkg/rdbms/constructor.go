2733d1b88ee3dcdf2b665931832dcd91998ceca7
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-05-01 20:48:14 +0900

    fix constructor

--- pkg/rdbms/constructor.go
+++ pkg/rdbms/constructor.go
@@ -3,14 +3,11 @@ package rdbms
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 )
 
 
-
-type Psql struct {
-	*sql.DB
-}
 
 var (
 	PSQL_HOST     = os.Getenv("PSQL_HOST")
@@ -20,9 +17,37 @@ var (
 	PSQL_DATABASE = os.Getenv("PSQL_DATABASE")
 )
 
-var instance *sql.DB
-
 func init() {
+
+	if _, exist := os.LookupEnv("PSQL_HOST"); !exist {
+		log.Fatalf("error: %s enveironment variable required", "PSQL_HOST")
+	}
+
+	if _, exist := os.LookupEnv("PSQL_PORT"); !exist {
+		log.Fatalf("error: %s enveironment variable required", "PSQL_PORT")
+	}
+
+	if _, exist := os.LookupEnv("PSQL_USER"); !exist {
+		log.Fatalf("error: %s enveironment variable required", "PSQL_USER")
+	}
+
+	if _, exist := os.LookupEnv("PSQL_PASS"); !exist {
+		log.Fatalf("error: %s enveironment variable required", "PSQL_PASS")
+	}
+
+	if _, exist := os.LookupEnv("PSQL_DATABASE"); !exist {
+		log.Fatalf("error: %s enveironment variable required", "PSQL_DATABASE")
+	}
+}
+
+
+
+type PSQL struct {
+	*sql.DB
+}
+
+
+func NewInstance() *PSQL {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 	PSQL_HOST, PSQL_PORT, PSQL_USER, PSQL_PASS, PSQL_DATABASE)
 	
@@ -31,23 +56,13 @@ func init() {
 	if err != nil {
 		panic(db)
 	}
-	
-	instance = db
+
+	return &PSQL{DB: db}
 }
 
 
-
-func NewInstance() *sql.DB {
-	return instance
-}
-
-func Close() error {
-
-	if err := instance.Close(); err != nil {
-		return err
-	}
-	
-	return nil
+func (p *PSQL) Close() error {
+	return p.DB.Close()
 }
 
 
