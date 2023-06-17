5bd4cf61986b5e0d4791f87087c521748d243c64
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-06-18 00:51:57 +0900

    create topic packing/unpacking source and test code

--- pkg/broker/config.go
+++ pkg/broker/config.go
@@ -1,7 +1,22 @@
 package broker
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 
 
 var defaultTimeout = time.Second * 3
+
+
+
+var prefix string = "broker:"
+
+func packTopic(topic string) string {
+	return prefix + topic
+}
+
+func unpackTopic(topic string) string {
+	return strings.TrimPrefix(topic, prefix)
+}
